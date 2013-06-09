5ef7b060d9d636311a5ce9b8f1a45df2c1e6d39a
manish <manu.juneja@gmail.com>
2013-06-10 00:42:16 +0530

    fifth commit

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,10 +1,10 @@
 #include "testApp.h"
 
-float p_x;
-float p_y;
-float p_r;
-float e_x;
-float e_y;
+float pacman_position_x;
+float pacman_position_y;
+float pacman_radius;
+float enemy_position_x;
+float enemy_position_y;
 int CounterForGameOver;
 
 //--------------------------------------------------------------
@@ -20,11 +20,11 @@ void testApp::setup(){
 	mySoundMovePacman.setMultiPlay(false);
 	// mySoundMovePacman.setLoop(true);
 
-	p_x = 400;
-	p_y = 500;
-	p_r = 15;
-    e_x = 375;
-	e_y = 175;
+	pacman_position_x = 400;
+	pacman_position_y = 500;
+	pacman_radius = 15;
+    enemy_position_x = 375;
+	enemy_position_y = 175;
 
 	CounterForGameOver=0;
 
@@ -48,10 +48,10 @@ void testApp::setup(){
 
 	
 	
-	/*if((p_x == e_x+12.5) || ( p_y+7.5 == e_y) || (p_y-7.5 == e_y+25)  || (p_x -7.5 == e_x+25) || (p_y == e_y+12.5) || (p_x+7.5 == e_x) || ( p_y ==e_y+12.5)  )
+	/*if((pacman_position_x == enemy_position_x+12.5) || ( pacman_position_y+7.5 == enemy_position_y) || (pacman_position_y-7.5 == enemy_position_y+25)  || (pacman_position_x -7.5 == enemy_position_x+25) || (pacman_position_y == enemy_position_y+12.5) || (pacman_position_x+7.5 == enemy_position_x) || ( pacman_position_y ==enemy_position_y+12.5)  )
 			{
-				p_x = 400;
-				p_y = 500;
+				pacman_position_x = 400;
+				pacman_position_y = 500;
 			}
 			*/
 
@@ -73,11 +73,11 @@ void testApp::draw(){
 
 	ofSetColor(128,128,0);
 	ofFill();
-	ofRect(e_x,e_y,30,30);
+	ofRect(enemy_position_x,enemy_position_y,30,30);
 
 	ofSetColor(128,128,0);
 	ofFill();
-	ofCircle(p_x, p_y ,p_r);
+	ofCircle(pacman_position_x, pacman_position_y ,pacman_radius);
 	///------------------------copied code---
 	 for (int i = 1; i < 20; i++) {
 		for (int j = 1; j < 15; j++) {
@@ -105,35 +105,35 @@ void testApp::draw(){
 
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(e_x, e_y+15 ,1);
+	ofCircle(enemy_position_x, enemy_position_y+15 ,1);
 
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(e_x+15, e_y ,1);
+	ofCircle(enemy_position_x+15, enemy_position_y ,1);
 
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(e_x+30, e_y+15 ,1);
+	ofCircle(enemy_position_x+30, enemy_position_y+15 ,1);
 
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(e_x+15, e_y+30 ,1);
+	ofCircle(enemy_position_x+15, enemy_position_y+30 ,1);
 
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(p_x+15, p_y ,1);
+	ofCircle(pacman_position_x+15, pacman_position_y ,1);
 	
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(p_x-15, p_y ,1);
+	ofCircle(pacman_position_x-15, pacman_position_y ,1);
 
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(p_x, p_y+15 ,1);
+	ofCircle(pacman_position_x, pacman_position_y+15 ,1);
 	
 	ofSetColor(127,255,0);
 	ofFill();
-	ofCircle(p_x, p_y-15 ,1);
+	ofCircle(pacman_position_x, pacman_position_y-15 ,1);
 	
 //-- testing code over ----------------------------
 	
@@ -159,15 +159,15 @@ void testApp::keyPressed(int key)
 
 	 if((key == 'w') || (key == 'W') || (key == 's') || (key == 'S') || (key == 'A') || (key == 'a') || (key == 'd') || (key == 'D')) 
 		{
-		/*	if((p_x == e_x+12.5) || ( p_y+7.5 == e_y) || (p_y-7.5 == e_y+25)  || (p_x -7.5 == e_x+25) || (p_y == e_y+12.5) || (p_x+7.5 == e_x) || ( p_y ==e_y+12.5)  )
+		/*	if((pacman_position_x == enemy_position_x+12.5) || ( pacman_position_y+7.5 == enemy_position_y) || (pacman_position_y-7.5 == enemy_position_y+25)  || (pacman_position_x -7.5 == enemy_position_x+25) || (pacman_position_y == enemy_position_y+12.5) || (pacman_position_x+7.5 == enemy_position_x) || ( pacman_position_y ==enemy_position_y+12.5)  )
 			{
-				p_x = 400;
-				p_y = 500;
+				pacman_position_x = 400;
+				pacman_position_y = 500;
 			}
 
  */		//--- loop for simple collision detetction---------------------
 
-			 if(((p_x+15 == e_x) && ( p_y == e_y+15)) || ((p_x-15 == e_x+30) && ( p_y == e_y+15)) || ((p_x == e_x+15) && ( p_y-15 == e_y+30)) || ((p_x == e_x+15) && ( p_y+15 == e_y))   )
+			 if(((pacman_position_x+15 == enemy_position_x) && ( pacman_position_y == enemy_position_y+15)) || ((pacman_position_x-15 == enemy_position_x+30) && ( pacman_position_y == enemy_position_y+15)) || ((pacman_position_x == enemy_position_x+15) && ( pacman_position_y-15 == enemy_position_y+30)) || ((pacman_position_x == enemy_position_x+15) && ( pacman_position_y+15 == enemy_position_y))   )
 			 {
 				 // if collision takes place play this sound ------
 
@@ -183,8 +183,8 @@ void testApp::keyPressed(int key)
 //------wait for 1900 microseconds for the sound to play and then repostion pacman on required (x,y)------------
 					Sleep(1900);
 
-					p_x = 400;
-					p_y = 500;
+					pacman_position_x = 400;
+					pacman_position_y = 500;
 
 					//adding counter for game over -------------
 
@@ -214,13 +214,13 @@ void testApp::keyPressed(int key)
 	 if((key == 'w') || (key == 'W'))
 	 {
  
-			if(p_y < -7.5)
+			if(pacman_position_y < -7.5)
 			{
-				p_y=p_y+607.5;
+				pacman_position_y=pacman_position_y+607.5;
 			}
 
 			else	{	
-				p_y = p_y - 5;
+				pacman_position_y = pacman_position_y - 5;
 			}
 			    
 			
@@ -229,13 +229,13 @@ void testApp::keyPressed(int key)
  if((key =='s') || (key =='S')) // 's' key
 		{
 			
-			if(p_y > 607.5)
+			if(pacman_position_y > 607.5)
 			{
-				p_y = p_y -607.5;
+				pacman_position_y = pacman_position_y -607.5;
 			}
 			else
 			{
-				p_y = p_y + 5;
+				pacman_position_y = pacman_position_y + 5;
 			}
 
 		
@@ -243,25 +243,25 @@ void testApp::keyPressed(int key)
  
  if((key =='d') || (key =='D')) // 'd' key
 		{
-			if(p_x > 807.5)
+			if(pacman_position_x > 807.5)
 			{
-				p_x = p_x -807.5;
+				pacman_position_x = pacman_position_x -807.5;
 			}
 			else
 			{
-				p_x = p_x + 5;
+				pacman_position_x = pacman_position_x + 5;
 			}
 		}
  
  if((key=='a') || (key == 'A')) // 'a' key
 		{
-			if(p_x < -7.5)
+			if(pacman_position_x < -7.5)
 			{
-				p_x = p_x + 807.5;
+				pacman_position_x = pacman_position_x + 807.5;
 			}
 			else
 			{
-				p_x = p_x - 5;
+				pacman_position_x = pacman_position_x - 5;
 			}
 			
 		}
